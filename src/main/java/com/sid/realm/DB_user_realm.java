6c085ccf1ac0f505de0e0fd6819aac2eaa5f46e0
Florian Grote <florian.grote@lobbe.de>
2019-12-28 17:47:05 +0100

    fixed leaking user roles

--- src/main/java/com/sid/realm/DB_user_realm.java
+++ src/main/java/com/sid/realm/DB_user_realm.java
@@ -22,9 +22,6 @@ public class DB_user_realm extends RealmBase {
     private static final Logger log = Logger.getLogger(DB_user_realm.class.getName());
     private String username;
     private String password;
-    private String DBUser; /* Proxy Datebank Account */
-    private String DBPassword; /* Proxy Datanebank account */
-    // private String DBRole;  /* Manuelle DBRollen vergabe */
     private String DBUrl;
     private String DBUserColumn = "USERNAME";
     private String DBRoleColumn = "granted_role";
@@ -33,8 +30,16 @@ public class DB_user_realm extends RealmBase {
     private String RoleExcludes = "";
     private List<String> UserRoles = new ArrayList<String>();
 
+    private void reset() {
+        this.setAllRolesMode("strict");
+        UserRoles = new ArrayList<String>();
+        this.username = null;
+        this.password = null;
+    }
+
     @Override
     public Principal authenticate(String username, String credentials) {
+        this.reset();
         this.username = username;
         this.password = credentials;
         try {
@@ -56,12 +61,6 @@ public class DB_user_realm extends RealmBase {
 
     @Override
     protected Principal getPrincipal(String string) {
-        // Setzt die Rollen manuel
-        /*
-        List<String> roles = new ArrayList<String>();
-        roles.add( role );
-        Principal principal = new GenericPrincipal(this.username, this.password, roles);
-        */
         Principal principal = new GenericPrincipal(this.username, this.password, UserRoles);
         return principal;
     }
@@ -69,54 +68,34 @@ public class DB_user_realm extends RealmBase {
     private Principal connectProxy() throws SQLException {
         Properties info = new Properties();
         // Hier wird der Benutzer als Connection gesetzt)
-        info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, this.username /*this.getDBUser()*/ );
-        info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, this.password /*this.getDBPassword()*/ ); 
-
-        // Man könnte weiterhin eine Proxy Connection nutzen um die Rechte einzuschränken?!
-        // Das braucht es nicht mehr
-        /*
-        Properties prop = new Properties();
-        prop.put(OracleConnection.PROXY_USER_NAME, this.username); 
-        prop.put(OracleConnection.PROXY_USER_PASSWORD, this.password); 
-        String[] roles = {this.getDBRole() };
-        prop.put(OracleConnection.PROXY_ROLES, roles);
-        */
-        // Bis hier hin
+        info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, this.username);
+        info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, this.password); 
 
         OracleDataSource ods = new OracleDataSource();
         ods.setURL( this.getDBUrl() );    
         ods.setConnectionProperties(info);
 
         try (OracleConnection connection = (OracleConnection) ods.getConnection()) {
-            // Man könnte weiterhin eine Proxy Connection nutzen um die Rechte einzuschränken?!
-            // Proxy connection braucht es nicht mehr
-            /*
-            connection.openProxySession(OracleConnection.PROXYTYPE_USER_NAME, prop); 
-            if (connection.isProxySession() ) {
-                log.info("Authentication is taking place for the user: " + this.username);
-                connection.close(OracleConnection.PROXY_SESSION);
-                connection.close();    
-                return getPrincipal(this.username);
-            } else {
-                return null;
-            }
-            */
-
-            //Stattdessen ein SQL um die Rollen abzufragen und diese in den Principle setzen.
             Statement statement = connection.createStatement();
 
             // Name ist Case insensitive
-            try (ResultSet resultSet = statement.executeQuery( "select " + this.getDBRoleColumn() + 
+            try (ResultSet resultSet = statement.executeQuery(
+                "select " + this.getDBRoleColumn() + 
                 " from " + this.getDBRoleTable() + " where 1=1" +
                 " and lower(" + this.DBUserColumn + ") = '" + this.getName().toLowerCase() + "'"
                 + this.getRoleIncludes()
-                + this.getRoleExcludes() ) ) {
-                while (resultSet.next() ) { /* Moves forward */
-                    UserRoles.add(resultSet.getString(1) ); // case Sensitive
-                    log.finest("Authentication is taking place for the user: " + this.username + " Role(1): " + resultSet.getString(1) );
-                }
-                connection.close();
-                return getPrincipal( this.getName() );
+                + this.getRoleExcludes() 
+                )) {
+                    while (resultSet.next() ) { /* Moves forward */
+                        UserRoles.add(resultSet.getString(1) ); // case Sensitive
+                        log.fine("Authentication is taking place for the user: " + this.username + " Role(1): " + resultSet.getString(1) );
+                    }
+                    connection.close();
+                    if (UserRoles.isEmpty()) {
+                        log.fine("Authentication is taking place for the user: " + this.username + " No Roles found: " + UserRoles.isEmpty());
+                        return null;
+                    } 
+                    return getPrincipal( this.getName() );
             }
             catch(SQLException e2) {
                 connection.close();
@@ -133,20 +112,6 @@ public class DB_user_realm extends RealmBase {
     }
 
     /* Custom Properties */
-    public String getDBUser() {
-        return this.DBUser;
-    }
-    public void setDBUser(String DBUser) {
-        this.DBUser = DBUser;
-    }
-
-    public String getDBPassword() {
-        return this.DBPassword;
-    }
-    public void setDBPassword(String DBPassword) {
-        this.DBPassword = DBPassword;
-    }
-
     public String getDBUrl() {
         return this.DBUrl;
     }
@@ -188,14 +153,4 @@ public class DB_user_realm extends RealmBase {
     public void setRoleExcludes(String RoleExcludes) {
         this.RoleExcludes = " and " + this.getDBRoleColumn() + " not in ("  + RoleExcludes + ")";
     }
-
-    // Nur Nötig bei fester Rollen vergabe
-    /*
-    public String getDBRole() {
-        return this.DBRole;
-    }
-    public void setDBRole(String DBRole) {
-        this.DBRole = DBRole;
-    }
-    */
 }
